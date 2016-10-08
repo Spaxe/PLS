bd6563409e8f833a20e1f969508a3c04dad29a58
Xavier Ho <contact@xavierho.com>
2016-10-09 07:47:06 +1100

    Initial commit for registration

--- Registration/dev.sh
+++ Registration/dev.sh
@@ -1,4 +1,4 @@
 node_modules/.bin/watchify app/main.js -v -t [ babelify --presets [ es2015 ] ] -o app/public/bundle.js &
 
-node node_modules/.bin/http-server -p 8000 app/public
+node node_modules/.bin/http-server -p 8888 app/public
 
